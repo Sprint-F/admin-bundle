7f926b7b787fd1fa7ab2097c3ec690f5a72c93c0
Dmitry Tomak <tomakdg@gmail.com>
2025-02-03 20:39:03 +1000

    SPRINTF-15: использовал атрибут вместо устаревших аннотаций

--- src/Controller/AdminControllerIndexTrait.php
+++ src/Controller/AdminControllerIndexTrait.php
@@ -17,7 +17,7 @@ use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 /**
  * @mixin AbstractAdminController
